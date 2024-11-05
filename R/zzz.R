1c9f9be63994ab869bf96adc80981a6fe2e2af9d
lokpoon <poonchiulok@gmail.com>
2024-11-04 20:32:23 -0500

    -

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.onAttach <- function(libname, pkgname) {
+  message("MoonShineR v1.1 is tested on these dependency package versions:
+  - suncalc (<= 0.5.1),
+	- magrittr (<= 2.0.3),
+	- dplyr (<= 1.1.4),
+	- lubridate (<= 1.9.3),
+	- REdaS (<= 0.9.4),
+	- ggplot2 (<= 3.5.1)"
+  )
+}
