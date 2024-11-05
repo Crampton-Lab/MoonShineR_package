cdcb9a0f500e570fc2651a8b9a3be8be640ba36e
lokpoon <poonchiulok@gmail.com>
2024-11-04 20:55:32 -0500

    -

--- R/package-doc.R
+++ R/package-doc.R
@@ -1,13 +1,6 @@
 #' MoonShineR Package
 #'
-#' The \code{\link{MoonShineR}} package provides tools for analyzing and predicting 
-#' light levels, specifically designed for studies of lunar influence on behavior.
-#'
-#' This package includes the following main functions:
-#' \itemize{
-#'   \item \code{\link{plot_lux}}: Plots light levels over time.
-#'   \item \code{\link{predict_lux}}: Predicts future light levels based on historical data.
-#'}
+#' Description of what the package does.
 #'
 #' @docType package
 #' @name MoonShineR
